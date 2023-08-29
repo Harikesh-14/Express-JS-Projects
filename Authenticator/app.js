b102155aefa5c42dee9801ad3411656c66a09797
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-08-29 22:46:02 +0530

    Just the starting of the project. Not yet done anything in it

---
+++ Authenticator/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const userRegister = require('./db/userDetails')
+require('./db/conn')
+
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log(`Your server is running at port: ${port}`)
+})
