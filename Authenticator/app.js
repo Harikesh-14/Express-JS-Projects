a608d90b858c6ad304178955750d14506ec60f5e
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-08-31 14:42:15 +0530

    Basic Structure of the Authenticator

--- Authenticator/app.js
+++ Authenticator/app.js
@@ -8,11 +8,34 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// for public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// for ejs file
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'signIn.html'));
 });
+
+app.post('/', async (req, res) => {
+    try{
+        const emailID = req.body.signInEmail
+        const password = req.body.signInPassword
+        const userFile = await userRegister.findOne({emailID, password})
+
+        if(userFile){
+            res.render('dashboard')
+        } else{
+            res.status(401).send("Invalid Username or Password")
+        }
+    } catch(err){
+        console.log(err)
+        res.status(500).send("Internal Server Error")
+    }
+})
 
 app.listen(port, () => {
     console.log(`Your server is running at port: ${port}`);
