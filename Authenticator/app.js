d34d6a8c421aaa5787b5e80ce3ca5e1491bab827
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-09-02 18:52:24 +0530

    ERRROR

--- Authenticator/app.js
+++ Authenticator/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const expressSession = require('express-session')
 const userRegister = require('./db/userDetails');
 const passport = require('passport');
 const { initializePassport } = require('./passportConfig');
@@ -8,7 +9,16 @@ require('./db/conn');
 
 const port = 3000;
 
+// setting up express-session
+app.use(expressSession({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+}))
+
 initializePassport(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,21 +34,21 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'signIn.html'));
 });
 
-app.post('/', async (req, res) => {
-    try{
-        const emailID = req.body.signInEmail
-        const password = req.body.signInPassword
-        const userFile = await userRegister.findOne({emailID, password})
+app.post('/', passport.authenticate("local"), async (req, res) => {
+    // try{
+    //     const emailID = req.body.signInEmail
+    //     const password = req.body.signInPassword
+    //     const userFile = await userRegister.findOne({emailID, password})
 
-        if(userFile){
-            res.render('dashboard')
-        } else{
-            res.status(401).send("Invalid Username or Password")
-        }
-    } catch(err){
-        console.log(err)
-        res.status(500).send("Internal Server Error")
-    }
+    //     if(userFile){
+    //         res.render('dashboard')
+    //     } else{
+    //         res.status(401).send("Invalid Username or Password")
+    //     }
+    // } catch(err){
+    //     console.log(err)
+    //     res.status(500).send("Internal Server Error")
+    // }
 })
 
 app.listen(port, () => {
--- Authenticator/passportConfig.js
+++ Authenticator/passportConfig.js
@@ -1,22 +1,42 @@
-const LocalStrategy = require('passport-local').Strategy
-const { User } = require('./db/userDetails')
+const LocalStrategy = require('passport-local').Strategy;
+const { User } = require('./db/userDetails');
 
 exports.initializePassport = (passport) => {
-    passport.use(new LocalStrategy(async (username, password, done) => {
-        const user = await User.findOne({ username })
+    passport.use(new LocalStrategy({
+        usernameField: "signInEmail",
+        passwordField: "signInPassword"
+    }, async (username, password, done) => {
+        const user = await User.findOne({ username });
 
         try {
             if (!user) {
-                return done(null, false)
+                return done(null, false);
             }
 
             if (user.password !== password) {
-                return done(null, false)
+                return done(null, false);
             }
-            return done(null, user)
+            return done(null, user);
         } catch (err) {
-            console.log(err)
-            return done(error, false)
+            console.log(err);
+            return done(err, false);
         }
-    }))
-}
+    }));
+
+    // mandatory step serializing and deserializing the user
+
+    // serializing means to save the user id until user logouts
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    // deserializing means to remove the user from the session after user logouts
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);  // Use findById instead of findByID
+            done(null, user);
+        } catch (err) {
+            done(err, false);
+        }
+    });
+};
